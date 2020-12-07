126c48782b2e7478e54cb3c50d4ebf08a9e63a62
Olivér Reider <reider340@gmail.com>
2020-12-07 21:30:15 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Name My Function
+
+Naming convention will: Help you organize, track, and find the data you have collected Prevent confusion among other researchers who may also be creating and modifying files Make it easier for other researchers to find and interpret your data.
+
+This simple extension will help you to name your functions consistently.
+
+## 🏃 Getting Started
+
+You can download Name My Function from the [VSCode Marketplace.](https://marketplace.visualstudio.com/vscode)
+
+### Usage:
+
+## ![Basic Usage](https://github.com/oli799/name-my-function/blob/main/assets/basic-usage.gif)
+
+## 🤝 Contributing
+
+Pull requests are welcome.
+
+---
+
+Give a ⭐️ if you like it!
---
+++ src/options.ts
@@ -0,0 +1,39 @@
+import * as vscode from 'vscode';
+
+export const funcTypes: vscode.QuickPickItem[] = [
+    'Pure',
+    'Side-effect',
+].map((label) => ({ label }));
+
+export const pureActionTypes: vscode.QuickPickItem[] = [
+    'Get',
+    'Add',
+    'Format',
+    'Remove',
+    'Request',
+    'Retrive',
+].map((label) => ({ label }));
+
+export const pureReturnTypes: vscode.QuickPickItem[] = [
+    'List',
+    'Boolean',
+    'Object',
+    'Number',
+    'Date',
+].map((label) => ({ label }));
+
+export const sideEffectActionTypes: vscode.QuickPickItem[] = [
+    'Detach',
+    'Dispatch',
+    'Initialize',
+    'Invoke',
+    'Set',
+    'Take',
+    'Go',
+].map((label) => ({ label }));
+
+export const trueFalseType: vscode.QuickPickItem[] = ['Yes', 'No'].map(
+    (label) => ({
+        label,
+    })
+);
